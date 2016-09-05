259d188a42bea3b7b4152003b1a9e26e729eb645
Colin McKean <ezikill@hotmail.com>
2016-09-05 11:05:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var animals = ["dog", "fish", "cat"];
+
+function changeCompletely(element, index, array){
+  array[index] = (Math.random()* 100).toString()+ '!!!'
+}
+
+function doToElementsInArray(array, callback){
+
+}
+
+
+// function changeCompletely(element, index, array) {
+//   array[index] = (Math.random() * 100).toString() + '!!!'
+// }
+//
+// var animals = ["dog", "fish", "cat"]
